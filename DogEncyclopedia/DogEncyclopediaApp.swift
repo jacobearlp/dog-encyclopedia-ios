93be983abcd7fd4120eba6888055e86e94d0e791
Jacob Earl <jacobearl.potolin@gmail.com>
2023-02-10 09:28:22 +0800

    Push basecode

---
+++ DogEncyclopedia/DogEncyclopediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogEncyclopediaApp.swift
+//  DogEncyclopedia
+//
+//  Created by Jacob on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DogEncyclopediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
